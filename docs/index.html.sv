770100b00690ae05737a37069e00b4c6c689446e
Esti Koen <ekoen@cray.com>
2018-11-15 11:53:21 -0600

    init

---
+++ docs/index.html.sv
@@ -0,0 +1,18 @@
+<!DOCTYPE html2
+<head>
+<title>Shasta System Management APIs</title>
+<body>
+
+  <h1>Shasta System Management Services</h1>
+
+  <a href="l1/index.html">HaaS</a>
+  <a href="l1/index.html">IaaS</a>
+  <a href="l1/index.html">PaaS</a>
+
+
+    <h2>Intro</h2>
+    Intro
+</body>
+</html>
+
+
